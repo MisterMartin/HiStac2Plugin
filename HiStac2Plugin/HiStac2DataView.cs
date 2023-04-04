d018d3e06efc627ae025a707005c3035ca4e8886
Charlie Martin <sugartechllc@gmail.com>
2023-04-03 20:49:12 -0700

    Configure the parameter formatting on the form

--- HiStac2Plugin/HiStac2DataView.cs
+++ HiStac2Plugin/HiStac2DataView.cs
@@ -110,35 +110,35 @@ namespace HiStack2Plugin
             }
             else
             {
-                STAC1_300Label.Text = string.Format("{0:0.00} [deg C]", STAC1_300);
-                STAC1_300_500Label.Text = string.Format("{0:0.00} [deg C]", STAC1_300_500);
-                STAC1_500_700Label.Text = string.Format("{0:0.00} [deg C]", STAC1_500_700);
-                STAC1_700_1000Label.Text = string.Format("{0:0.00} [deg C]", STAC1_700_1000);
-                STAC1_1000_3000Label.Text = string.Format("{0:0.00} [deg C]", STAC1_1000_3000);
-                STAC1_Pump1CurrentLabel.Text = string.Format("{0:0.00} [deg C]", STAC1_Pump1);
-                STAC1_Pump2CurrentLabel.Text = string.Format("{0:0.00} [deg C]", STAC1_Pump2);
-                STAC1_Pump1_2TempLabel.Text = string.Format("{0:0.00} [deg C]", STAC1_Pump1_2Temp);
-                STAC1_VBatLabel.Text = string.Format("{0:0.00} [deg C]", STAC1_VBat);
-                STAC1_SaturatorTempLabel.Text = string.Format("{0:0.00} [deg C]", STAC1_SatTemp);
-                STAC1_IceJacketTempLabel.Text = string.Format("{0:0.00} [deg C]", STAC1_IceJacketTemp);
-                STAC1_AltSourceLabel.Text = string.Format("{0:0.00} [deg C]", STAC1_AltStatus);
+                STAC1_300Label.Text           = string.Format("{0:0.000} [#/cc]", STAC1_300);
+                STAC1_300_500Label.Text       = string.Format("{0:0.000} [#/cc]", STAC1_300_500);
+                STAC1_500_700Label.Text       = string.Format("{0:0.000} [#/cc]", STAC1_500_700);
+                STAC1_700_1000Label.Text      = string.Format("{0:0.000} [#/cc]", STAC1_700_1000);
+                STAC1_1000_3000Label.Text     = string.Format("{0:0.000} [#/cc]", STAC1_1000_3000);
+                STAC1_Pump1CurrentLabel.Text  = string.Format("{0:0.00} [mA]",    STAC1_Pump1);
+                STAC1_Pump2CurrentLabel.Text  = string.Format("{0:0.00} [mA]",    STAC1_Pump2);
+                STAC1_Pump1_2TempLabel.Text   = string.Format("{0:0.0} [C]",      STAC1_Pump1_2Temp);
+                STAC1_VBatLabel.Text          = string.Format("{0:0.00} V]",      STAC1_VBat);
+                STAC1_SaturatorTempLabel.Text = string.Format("{0:0.0} [deg C]",  STAC1_SatTemp);
+                STAC1_IceJacketTempLabel.Text = string.Format("{0:0.0} [deg C]",  STAC1_IceJacketTemp);
+                STAC1_AltSourceLabel.Text     = string.Format("{0:0.00} [0,1,2]", STAC1_AltStatus);
 
-                STAC2_300Label.Text = string.Format("{0:0.00} [deg C]", STAC2_300);
-                STAC2_300_500Label.Text = string.Format("{0:0.00} [deg C]", STAC2_300_500);
-                STAC2_500_700Label.Text = string.Format("{0:0.00} [deg C]", STAC2_500_700);
-                STAC2_700_1000Label.Text = string.Format("{0:0.00} [deg C]", STAC2_700_1000);
-                STAC2_1000_3000Label.Text = string.Format("{0:0.00} [deg C]", STAC2_1000_3000);
-                STAC2_Pump1CurrentLabel.Text = string.Format("{0:0.00} [deg C]", STAC2_Pump1);
-                STAC2_Pump2CurrentLabel.Text = string.Format("{0:0.00} [deg C]", STAC2_Pump2);
-                STAC2_Pump1_2TempLabel.Text = string.Format("{0:0.00} [deg C]", STAC2_Pump1_2Temp);
-                STAC2_VBatLabel.Text = string.Format("{0:0.00} [deg C]", STAC2_VBat);
-                STAC2_SaturatorTempLabel.Text = string.Format("{0:0.00} [deg C]", STAC2_SatTemp);
-                STAC2_CondenserTempLabel.Text = string.Format("{0:0.00} [deg C]", STAC2_CondenserTemp);
+                STAC2_300Label.Text           = string.Format("{0:0.000} [#/cc]", STAC2_300);
+                STAC2_300_500Label.Text       = string.Format("{0:0.000} [#/cc]", STAC2_300_500);
+                STAC2_500_700Label.Text       = string.Format("{0:0.000} [#/cc]", STAC2_500_700);
+                STAC2_700_1000Label.Text      = string.Format("{0:0.000} [#/cc]", STAC2_700_1000);
+                STAC2_1000_3000Label.Text     = string.Format("{0:0.000} [#/cc]", STAC2_1000_3000);
+                STAC2_Pump1CurrentLabel.Text  = string.Format("{0:0.00} [mA]",    STAC2_Pump1);
+                STAC2_Pump2CurrentLabel.Text  = string.Format("{0:0.00} [mA]",    STAC2_Pump2);
+                STAC2_Pump1_2TempLabel.Text   = string.Format("{0:0.0} [deg C]",  STAC2_Pump1_2Temp);
+                STAC2_VBatLabel.Text          = string.Format("{0:0.00} [V]",     STAC2_VBat);
+                STAC2_SaturatorTempLabel.Text = string.Format("{0:0.0} [deg C]",  STAC2_SatTemp);
+                STAC2_CondenserTempLabel.Text = string.Format("{0:0.0} [deg C]",  STAC2_CondenserTemp);
 
-                HI_UpstreamTempLabel.Text = string.Format("{0:0.00} [deg C]", HI_UpstreamTemp);
-                HI_MidstreamTempLabel.Text = string.Format("{0:0.00} [deg C]", HI_MidstreamTemp);
-                HI_DownstreamTempLabel.Text = string.Format("{0:0.00} [deg C]", HI_DownstreamTemp);
-                HI_BatVLabel.Text = string.Format("{0:0.00} [deg C]", HI_VBat);
+                HI_UpstreamTempLabel.Text     = string.Format("{0:0.0} [deg C]",  HI_UpstreamTemp);
+                HI_MidstreamTempLabel.Text    = string.Format("{0:0.0} [deg C]",  HI_MidstreamTemp);
+                HI_DownstreamTempLabel.Text   = string.Format("{0:0.0} [deg C]",  HI_DownstreamTemp);
+                HI_BatVLabel.Text             = string.Format("{0:0.00} [V]",     HI_VBat);
             }
         }
     }
